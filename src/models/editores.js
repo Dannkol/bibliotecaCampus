8af5e53769fb80ed84cf149f4a15ebc7e5f48473
Daniel Manosalva <Manosalvasierra@gmail.com>
2023-07-24 07:22:42 -0500

    feat: :sparkles: Tercer endpoint
    Mostrar todas las editoriales y sus direcciones
    
    dannkol

---
+++ src/models/editores.js
@@ -0,0 +1,36 @@
+import mysql from "mysql2/promise";
+
+import dbConfig from "../config/dbconfig.js";
+
+/* instacia de la conexion a la base de datos */
+const getConnection = async () => {
+  return await mysql.createConnection(dbConfig);
+};
+
+
+const getAlleditor = async () => {
+
+    const Connection = await getConnection();
+
+    try {
+        
+        const query = `SELECT nombre, direccion FROM editorial;`
+
+        const query_get = await Connection.execute(query);
+
+        return {
+            mensaje : "Todas las editoriales",
+            data : query_get
+        }
+
+
+    } catch (error) {
+        
+    }finally{
+        Connection.end();
+    }
+
+
+}
+
+export default { getAlleditor }
--- src/routes/publics.js
+++ src/routes/publics.js
@@ -11,10 +11,14 @@ const router = express.Router();
 
 import { getAllautores } from "../controllers/autoresController.js";
 import { getAllcategorias } from "../controllers/categoriasController.js";
+import { getAlleditor } from "../controllers/editoresController.js";
 
 router.get('/autores', getAllautores);
 
 router.get('/categorias', getAllcategorias);
+
+router.get('/editorial', getAlleditor);
+
 
 
 
